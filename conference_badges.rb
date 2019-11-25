181c92c545dc5f83d30c2e6cda6baa43bd51f011
celebrated-integer-2023 <scailles23@gmail.com>
2019-11-25 09:32:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-ttendees=["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+attendees=["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
 def batch_badge_creator(attendees)
   
   
